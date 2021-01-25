1b697d803d19efed89a69e31bb3262d0eab31104
Anton <a.kolesnikov@niitp.in>
2021-01-25 11:24:23 +0300

    migrate project to hibernate4

--- src/main/java/ru/homework/cars/server/dao/AbstractDAOClass.java
+++ src/main/java/ru/homework/cars/server/dao/AbstractDAOClass.java
@@ -1,14 +1,28 @@
 package ru.homework.cars.server.dao;
 
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import ru.homework.cars.server.dao.interfaces.AbstractDaoInterface;
 import ru.homework.cars.server.data.AbstractEntity;
 
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
 import java.util.List;
-
+@Getter
+@Setter
 public class AbstractDAOClass implements AbstractDaoInterface {
 
+    protected SessionFactory sessionFactory;
     @Override
     public void save(AbstractEntity data) throws Exception {
+        Session session = this.sessionFactory.openSession();
+        Transaction tx = session.beginTransaction();
+        session.persist(data);
+        tx.commit();
+        session.close();
 
     }
 
@@ -19,6 +33,13 @@ public class AbstractDAOClass implements AbstractDaoInterface {
 
     @Override
     public List<AbstractEntity> getAll(Class aClass) throws Exception {
+        List<AbstractEntity> data = null;
+
+
+        Session session = this.sessionFactory.openSession();
+        CriteriaBuilder builder = session.getCriteriaBuilder();
+        
+        session.close();
         return null;
     }
 
