5ec9052e7552338ead8530c86d1911a9c09187d5
Anton <a.kolesnikov@niitp.in>
2021-02-03 10:21:07 +0300

    update pom.xml

--- src/main/java/ru/homework/cars/server/MainClass.java
+++ src/main/java/ru/homework/cars/server/MainClass.java
@@ -2,9 +2,8 @@ package ru.homework.cars.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScans;
 
-@SpringBootApplication(scanBasePackages = "ru.homework.cars.server")
+@SpringBootApplication
 public class MainClass {
 
     public static void main(String[] args) {
